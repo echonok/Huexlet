1535a16c66dbedd38d817d30b5ace25616753959
Nick Elovsky <echonok@gmail.com>
2020-03-18 09:29:22 +0300

    ~

---
+++ 3. JS Массивы/2. Синтаксис/arrays.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-param-reassign */
+
+const apply = (arr, operationName, index, value) => {
+  // BEGIN (write your solution here)
+
+
+  }
+  // END
+};
+
+export default apply;
